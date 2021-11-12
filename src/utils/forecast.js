3d2a524581d947dc3f83829f2fe94dc4d5ce017f
Abhijeet Shyam Jagtap <aj.abhijeet21@hotmail.com>
2021-11-12 21:01:05 +0530

    Added feelslike value on forecast page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,8 +13,9 @@ const forecast = (longitude, lattitude, callback) => {
             // console.log(url);
             const result = `Weather description: ${body.current.weather_descriptions[0]}
             It it currently ${body.current.temperature} degrees out in ${body.location.name}.
-            There is a ${body.current.precip}% chance of rain.
-            Humidity is around ${body.current.humidity} and wind direction is towards ${body.current.wind_dir}`;
+            It feels like ${body.current.feelslike} degrees out.
+            Humidity is around ${body.current.humidity}% and wind direction is towards ${body.current.wind_dir}.
+            There is a ${body.current.precip}% chance of rain.`;
             callback(undefined, result);
         }
     });
